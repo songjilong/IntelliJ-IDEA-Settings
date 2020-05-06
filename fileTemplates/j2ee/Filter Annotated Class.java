59257b7f975fc1f4c4f7aa14b7cd4a538bde270d
songjilong <2606587750@qq.com>
2020-05-06 23:34:21 +0800

    IU-2019.3.3 <Lenovo@LAPTOP-LFAOC6JB Create markdown.xml, print.xml, project.default.xml, equalsHashCodeTemplates.xml, Default copy.xml, shkstart.xml, yiiguxing.translation.xml, Servlet Annotated Class.java, Default.xml, ignore.xml, find.xml, File Header.java, keymap.xml, _@user_Darcula.icls, mavenVersion.xml, Ladies Night 2.icls, EasyOnTheEyes.icls, Default.xml, gitee.xml, Eclipse copy.xml, editor.xml, uml.xml, debugger.xml, updates.xml, baseRefactoring.xml, changeReminder.user.storage.xml, ide.general.xml, other.xml, git.xml, _@user_Default.icls, lombok-plugin.xml, user.xml, ui.lnf.xml, xml.java, filetypes.xml, projectView.xml, javaeeExternalResources.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, github.xml, templates.xml, studioFlags.xml, laf.xml, jdk.table.xml, path.macros.xml, Filter Annotated Class.java, intentionSettings.xml, web-browsers.xml, wsl.distributions.xml, vcs.xml, Base16 Ocean.icls, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, setterTemplates.xml

---
+++ fileTemplates/j2ee/Filter Annotated Class.java
@@ -0,0 +1,18 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+@javax.servlet.annotation.WebFilter("/*")
+public class ${Class_Name} implements javax.servlet.Filter {
+
+    public void doFilter(javax.servlet.ServletRequest req, javax.servlet.ServletResponse resp, javax.servlet.FilterChain chain) throws javax.servlet.ServletException, java.io.IOException {
+        chain.doFilter(req, resp);
+    }
+
+    public void init(javax.servlet.FilterConfig config) throws javax.servlet.ServletException {
+
+    }
+    
+    public void destroy() {
+    
+    }
+
+}
